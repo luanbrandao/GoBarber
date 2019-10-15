29711b6a5f9b74c2d40e2c3cbee40c8cacca6af8
luanBrandao <luanbrandao4@gmail.com>
2019-10-15 17:14:03 -0300

    Configurando Sequelize

--- src/server.js
+++ src/server.js
@@ -5,5 +5,6 @@ import app from './app';
 // yarn eslint --init
 // yarn add prettier eslint-config-prettier eslint-plugin-prettier -D
 // yarn eslint --fix src --ext .js
-
+// yarn add sequelize
+// yarn add sequelize-cli -D
 app.listen(3333);
