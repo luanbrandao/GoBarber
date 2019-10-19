f988c42125cc51e2f37b81ae861a016b5f09b97a
luanBrandao <luanbrandao4@gmail.com>
2019-10-19 12:13:42 -0300

    Migration e model de agendamento

---
+++ API/src/database/migrations/20191019150330-create-appointments.js
@@ -0,0 +1,50 @@
+// yarn sequelize migration:create --name=create-appointments
+// yarn sequelize db:migrate
+// yarn sequelize db:migrate:undo
+// yarn sequelize  db:migrate:undo:all
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      // prestador do serviço
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      canceled_at: {
+        type: Sequelize.DATE
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('appointments');
+  }
+};
