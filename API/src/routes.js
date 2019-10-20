2afa88ee176834535c9840c166fc230bcdc8c2ff
luanBrandao <luanbrandao4@gmail.com>
2019-10-20 16:12:27 -0300

    Cancelamento de agendamento

--- API/src/routes.js
+++ API/src/routes.js
@@ -39,6 +39,8 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
