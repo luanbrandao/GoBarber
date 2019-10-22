140b35b1c608c09f79e1f0a9327999c0ae56f95a
luanBrandao <luanbrandao4@gmail.com>
2019-10-22 00:44:54 -0300

    Campos virtuais no agendamento

--- API/src/app/models/Appointment.js
+++ API/src/app/models/Appointment.js
@@ -1,11 +1,26 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
     super.init(
       {
         date: Sequelize.DATE,
-        canceled_at: Sequelize.DATE
+        canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            // verifica se a data do agendamento e inferior a data atual
+            return isBefore(this.date, new Date());
+          }
+        },
+        // se o angedamento e cancelável ou não
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          }
+        }
       },
       {
         sequelize
