7e180a7515e62604c94a8f434c51d280783cdc7f
luanBrandao <luanbrandao4@gmail.com>
2019-11-22 13:22:04 -0300

    ESLint, Prettier & EditorConfig

--- gobarber-web/src/index.js
+++ gobarber-web/src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// yarn add eslint -D
+// yarn eslint --init
+// yarn add prettier eslint-config-prettier eslint-plugin-prettier babel-eslint  -D
+// yarn add eslint-plugin-react-hooks -D
+// yarn add react-router-dom
+// yarn add styled-components
+// yarn add react-icons
 
 ReactDOM.render(<App />, document.getElementById('root'));
