50fc8c77674db315640ca78ec7b0f30f569c47be
luanBrandao <luanbrandao4@gmail.com>
2019-11-29 16:48:55 -0300

    WEB - Página de perfil

--- gobarber-web/src/pages/Profile/index.js
+++ gobarber-web/src/pages/Profile/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
-    <div>
-      <h1>profile</h1>
-    </div>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu endereçode e-mail" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair GoBarber</button>
+    </Container>
   );
 }
