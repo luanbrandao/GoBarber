fc61649106fc4fa4710ef86fd5d4ef4754d7a093
luanBrandao <luanbrandao4@gmail.com>
2019-12-18 20:52:53 -0300

    APP - Lista de agendamentos

--- app/src/pages/Dashboard/index.js
+++ app/src/pages/Dashboard/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-// import { Container } from './styles';
-
+import { Container, Title, List } from './styles';
+import Appointment from '~/components/Appointment';
 import Background from '~/components/Background';
 
+const data = [1, 2, 3, 4, 5];
+
 export default function Dashboard() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+      </Container>
+      <List
+        data={data}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Appointment data={item} />}
+      />
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
