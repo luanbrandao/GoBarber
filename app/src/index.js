0ba8320dc00708eff70378f379c3bb85e74c4af5
luanBrandao <luanbrandao4@gmail.com>
2019-12-18 01:31:11 -0300

    APP - config Reactotron

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import './config/ReactotronConfig';
 import Routes from './routes';
 // yarn add react-native-linear-gradient
 // react-native link react-native-linear-gradient
@@ -9,6 +10,8 @@ import Routes from './routes';
 // react-native-gesture-handler
 // yarn add react-navigation
 // yarn add react-native-reanimated react-native-gesture-handler react-native-screens@^1.0.0-alpha.23
+// yarn add reactotron-react-native reactotron-redux reactotron-redux-saga
+// yarn add redux  redux-saga react-redux
 export default function App() {
   return (
     <>
