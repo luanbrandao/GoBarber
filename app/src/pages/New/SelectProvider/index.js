c8c7f0f337873b3d3aea11a66cfdd4fc1bd96703
luanBrandao <luanbrandao4@gmail.com>
2019-12-21 21:39:49 -0300

    APP - Listagem de prestadores

--- app/src/pages/New/SelectProvider/index.js
+++ app/src/pages/New/SelectProvider/index.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
-// import { TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import Background from '~/components/Background';
-// import { Container } from './styles';
-export default function SelectProvider() {
-  return <Background />;
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+import api from '~/services/api';
+
+export default function SelectProvider({ navigation }) {
+  const [providers, setProvider] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+      setProvider(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={provider => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatars/50/${provider.name}`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        >
+          {' '}
+        </ProvidersList>
+      </Container>
+    </Background>
+  );
 }
 
 SelectProvider.navigationOptions = ({ navigation }) => ({
